cd8bad4b2fbddef2df19baef907049dd2793f212
Gregory Chris <sgchris@gmail.com>
2018-10-29 15:23:00 +0200

    added google analytics

--- index.php
+++ index.php
@@ -264,6 +264,15 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="assets/Vibrant.min.js"></script>
     <script src="assets/main.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-6335805-12', 'auto');
+        ga('send', 'pageview');
+    </script>
 </body>
 
 </html>
